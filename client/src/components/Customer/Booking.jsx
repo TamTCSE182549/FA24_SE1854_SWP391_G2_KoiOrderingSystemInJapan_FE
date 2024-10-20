39ac0f1f59b43f9f755f873ac2da61c34767635a
TranChiTam <tranchitam65a@gmail.com>
2024-10-20 20:42:40 +0700

    update remaning

--- client/src/components/Customer/Booking.jsx
+++ client/src/components/Customer/Booking.jsx
@@ -67,19 +67,24 @@ const BookingTrip = () => {
     };
 
     try {
-      const response = await axios.post(
-        "http://localhost:8080/bookings/CreateForTour",
-        bookingData,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`, // Ensure the token is correctly passed
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log("Booking successful:", response.data);
-      // NotificationManager.success("Booking successful!", "Success", 5000);
-      toast.success("Booking successful!");
+      if(participants<=tour.remaining){
+        const response = await axios.post(
+          "http://localhost:8080/bookings/CreateForTour",
+          bookingData,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`, // Ensure the token is correctly passed
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        console.log("Booking successful:", response.data);
+        // NotificationManager.success("Booking successful!", "Success", 5000);
+        toast.success("Booking successful!");
+      } else {
+        toast.warning("Participants must be less than or equal remaning of tour AND must be greater than 0");
+      }
+      
     } catch (error) {
       if (error.response) {
         console.error("Error response:", error.response.data);
@@ -162,6 +167,15 @@ const BookingTrip = () => {
             type="hidden"
             value={tourID} // Lưu tourID ẩn
             onChange={(e) => setTourID(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block font-semibold mb-2">Remaning of Tour:</label>
+          <input
+            value={tour.remaining} // Hiển thị tên tour
+            readOnly
+            className="border border-gray-300 rounded-md p-2 w-full bg-gray-100"
           />
         </div>
 
